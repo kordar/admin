267a827198f0e84352fbcd14e972d97987704142
perpel <572821520@qq.com>
2018-11-30 22:30:41 +0800

    first commit

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace kordar\admin;
+
+/**
+ * ace module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'kordar\admin\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
